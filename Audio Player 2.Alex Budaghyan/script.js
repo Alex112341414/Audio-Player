db343188c68394ca89012ff7ed8bc67cf6a1a228
Alex112341414 <140798313+Alex112341414@users.noreply.github.com>
2024-05-17 17:17:12 +0400

    Update script.js

--- Audio Player 2.Alex Budaghyan/script.js
+++ Audio Player 2.Alex Budaghyan/script.js
@@ -1,7 +1,24 @@
 let data = {
-    title:["MiyaGi - Самурай ","MiyaGi & Andy Panda - Minor","MiyaGi & Andy Panda - Там Ревели Горы","MiyaGi & Эндшпиль - Fire Man","Miyagi & Эндшпиль feat. Рем Дигга - I Got Love"],
-    song:["music/MiyaGi - Самурай.mp3","music/MiyaGi & Andy Panda - Minor.mp3","music/MiyaGi & Andy Panda - Там Ревели Горы.mp3","music/MiyaGi & Эндшпиль - Fire Man.mp3","music/Miyagi & Эндшпиль feat. Рем Дигга - I Got Love.mp3"],
-    poster:["https://i.pinimg.com/originals/3c/95/b7/3c95b76d0d7bd7f2379e6a7ac11d0983.jpg","https://sun9-78.userapi.com/impg/BmWQATsqBTev0pEObU0zenbBJ8rwAwQczxy88A/e8afJ7whn-8.jpg?size=807x727&quality=96&sign=c2fa8c358f32fd9065d2f02fbcea6feb&c_uniq_tag=2hulmpPA0Oy6Tjw0iQfgnMKGmNU-yWuVPwfEr_Q6aVI&type=album","https://images.genius.com/cf2f320a368e711c369764f0b62bf772.1000x1000x1.png","https://www.cevirce.com/lyrics/wp-content/uploads/2021/11/miyagi-%D1%8D%D0%BD%D0%B4%D1%88%D0%BF%D0%B8%D0%BB%D1%8C-fire-man-russisch-songtext-deutsch-uebersetzung-1.jpg","https://cdn1.tenchat.ru/static/vbc-gostinder/2022-05-26/181f4ade-5b2d-4099-84e5-02d289cf6841.jpeg"],
+    title:[ 
+        "MiyaGi - Самурай ",
+        "MiyaGi & Andy Panda - Minor",
+        "MiyaGi & Andy Panda - Там Ревели Горы",
+        "MiyaGi & Эндшпиль - Fire Man",
+        "Miyagi & Эндшпиль feat. Рем Дигга - I Got Love"
+    ],
+    song:[
+        "Music/MiyaGi - Самурай.mp3",
+        "Music/MiyaGi & Andy Panda - Minor.mp3",
+        "Music/MiyaGi & Andy Panda - Там Ревели Горы.mp3",
+        "Music/MiyaGi & Эндшпиль - Fire Man.mp3",
+        "Music/Miyagi & Эндшпиль feat. Рем Дигга - I Got Love.mp3"
+    ],
+    poster:[
+        "https://i.pinimg.com/originals/3c/95/b7/3c95b76d0d7bd7f2379e6a7ac11d0983.jpg",
+        "https://i.ytimg.com/vi/4EfM6rPmxow/maxresdefault.jpg",
+        "https://images.genius.com/cf2f320a368e711c369764f0b62bf772.1000x1000x1.png",
+        "https://www.cevirce.com/lyrics/wp-content/uploads/2021/11/miyagi-%D1%8D%D0%BD%D0%B4%D1%88%D0%BF%D0%B8%D0%BB%D1%8C-fire-man-russisch-songtext-deutsch-uebersetzung-1.jpg",
+        "https://cdn1.tenchat.ru/static/vbc-gostinder/2022-05-26/181f4ade-5b2d-4099-84e5-02d289cf6841.jpeg"],
 
 }
 
@@ -10,31 +27,31 @@ let song = new Audio()
 
 let currentSong = 0;
 
-
-
-function playSong(){
-    song.src = data.song[currentSong]
-    let = songTitle = document.getElementsByClassName("songtitle")
-    songTitle[0].textContent = data.title[currentSong];
-    let img = document.getElementsByClassName("row1")
-    img[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")"
-    let main = document.getElementsByClassName("main")
-    main.style.backgroundImage = "url(" + data.poster[currentSong] + ")"
-    // song.play()
-}
-
 window.onload = function(){
     playSong()
 }
 
 
 
+function playSong(){
+    console.log(currentSong);
+    song.src = data.song[currentSong]
+    console.log(song);
+    let  songTitle = document.getElementsByClassName("songtitle")
+    songTitle[0].textContent = data.title[currentSong];
+    let img = document.getElementsByClassName("row1")
+    img[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")"
+    let main = document.getElementsByClassName("main")
+    main[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")"
+    song.play()
+}
+    
 function playerPauseSong(){
     let play = document.getElementById("play")
 
     if(song.paused){
         song.play()
-        play.src = "images/pause.png"
+        play.src =  "images/pause.png"
     }else{
         song.pause()
         play.src = "images/play-button-arrowhead.png"
@@ -42,8 +59,8 @@ function playerPauseSong(){
 }
 
 song.addEventListener("timeupdate",function(){
- console.log(song.currentTime)
- console.log(song.duration)
+    //  console.log(song.currentTime)
+    //  console.log(song.duration)
 
  let fill = document.getElementsByClassName("fill")
 
@@ -93,4 +110,32 @@ function prev(){
         currentSong = 0
     }
     playSong()
+}
+
+
+function mute(){
+     mute = document.getElementById("mute")
+    if(song.muted){
+        song.muted = false
+        mute.src = "images/volume.png"
+
+    }else{
+        song.muted = true
+        mute.src = "images/volume-mute.png"
+    }
+}
+
+function decrease(){
+  
+    song.volume -= 0.2
+    if(song.volume <= 0.2){
+        song.volume = 0
+         mute.src = "images/volume-down.png"
+    }
+}
+
+function increase(){
+    song.volume += 0.2
+    mute.src = "images/volume.png"
+    
 }
